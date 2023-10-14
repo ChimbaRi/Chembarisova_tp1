dcc831a526f3ab56f3bb0067a9b65aab323c8439
ChimbaRi <magna7746@gmail.com>
2023-10-15 00:42:53 +0300

    Изменены классы Speakers, Administration и Conf_program

--- Laba_№1/Laba_№1/Administration.cpp
+++ Laba_№1/Laba_№1/Administration.cpp
@@ -1,1 +1,28 @@
 #include "Administration.h"
+void Administration::Set_Name(string n) { Conference::Set_Name(n); }
+void Administration::Set_Job_title(string jt) { this->Job_title = jt; }
+void Administration::Set_Area_of_responsibility(string aor) { this->Area_of_responsibility = aor; }
+
+string Administration::Get_Name() { return Conference::Get_Name(); }
+string Administration::Get_Job_title() { return Job_title; }
+string Administration::Get_Area_of_responsibility() { return Area_of_responsibility; }
+
+Administration::Administration() {
+	Set_Name(nullptr);
+	Set_Job_title(nullptr);
+	Set_Area_of_responsibility(nullptr);
+	cout << "Вызван конструктор без параметров для класса Administration" << endl;
+}
+Administration::Administration(string n, string jt, string aor) {
+	Set_Name(n);
+	Set_Job_title(jt);
+	Set_Area_of_responsibility(aor);
+	cout << "Вызван конструктор с параметрами для класса Administration" << endl;
+}
+Administration::Administration(const Administration& adm) {
+	Set_Name(adm.Name);
+	Set_Job_title(adm.Job_title);
+	Set_Area_of_responsibility(adm.Area_of_responsibility);
+	cout << "Вызван конструктор копирования для класса Administration" << endl;
+}
+Administration::~Administration() { cout << "Вызван деструктор для класса Administration" << endl; }
